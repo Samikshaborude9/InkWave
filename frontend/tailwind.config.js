3864426ad27d6301a93272aa24a6d15f8455626e
Samiksha Borude <samikshaborude9@gmail.com>
2025-11-22 18:12:24 +0530

    feat: font added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,12 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
+		
+      fontFamily: {
+        geist: ["Geist", "sans-serif"],
+        geistMono: ["Geist Mono", "monospace"],
+      },
+
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
